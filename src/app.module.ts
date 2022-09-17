9e95dc5a0076620f1c2a14061dc05a7d7bc1dc1e
ivanbrij <ji.barrerar1@uniandes.edu.co>
2022-09-17 16:02:31 +0000

    Adding city database in postgres for completing the persistence of the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,25 @@ import { AppService } from './app.service';
 import { CityModule } from './city/city.module';
 import { SupermarketModule } from './supermarket/supermarket.module';
 import { CitySupermarketModule } from './city-supermarket/city-supermarket.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CityEntity } from './city/city.entity';
+import { SupermarketEntity } from './supermarket/supermarket.entity';
 
 @Module({
-  imports: [CityModule, SupermarketModule, CitySupermarketModule],
+  imports: [CityModule, SupermarketModule, CitySupermarketModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'city',
+      entities: [CityEntity, SupermarketEntity],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
