22e9541a4084514ab40d27fce38df2c4ce620d0f
ivanbrij <ji.barrerar1@uniandes.edu.co>
2022-09-17 20:31:23 +0000

    Adding controllers for City, supermarket and City-Supermarket

--- src/city/city.controller.ts
+++ src/city/city.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
+import { CityDto } from './city.dto';
+import { CityEntity } from './city.entity';
+import { CityService } from './city.service';
 
-@Controller('city')
-export class CityController {}
+@Controller('cities')
+@UseInterceptors(BusinessErrorsInterceptor)
+export class CityController {
+    constructor(private readonly cityService: CityService) {}
+
+  @Get()
+  async findAll() {
+    return await this.cityService.findAll();
+  }
+
+  @Get(':cityId')
+  async findOne(@Param('cityId') cityId: string) {
+    return await this.cityService.findOne(cityId);
+  }
+
+  @Post()
+  async create(@Body() cityDto: CityDto) {
+    const city: CityEntity = plainToInstance(CityEntity, cityDto);
+    return await this.cityService.create(city);
+  }
+
+  @Put(':cityId')
+  async update(@Param('cityId') cityId: string, @Body() cityDto: CityDto) {
+    const city: CityEntity = plainToInstance(CityEntity, cityDto);
+    return await this.cityService.update(cityId, city);
+  }
+
+  @Delete(':cityId')
+  @HttpCode(204)
+  async delete(@Param('cityId') cityId: string) {
+    return await this.cityService.delete(cityId);
+  }
+}
